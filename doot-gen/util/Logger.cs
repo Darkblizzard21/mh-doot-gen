aa0e85fafcbd81bd2497592e0021045369306851
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2023-12-12 00:24:17 +0100

    Logger & Skins

---
+++ doot-gen/util/Logger.cs
@@ -0,0 +1,57 @@
+﻿namespace doot_gen.util
+{
+
+    internal class Logger
+    {
+        public static LogLevel Defaut
+        {
+            get
+            {
+#if DEBUG
+                return LogLevel.Debug;
+#else
+                return LogLevel.Info;
+#endif
+            }
+        }
+
+        public static void Debug(string message) { Log("DEBUG:  " +message, LogLevel.Debug); }
+        public static void Debug(string format, params object?[]? arg) { Log("DEBUG:  " + string.Format(format, arg), LogLevel.Debug); }
+        public static void Info(string message) { Log("INFO:   " + message, LogLevel.Info); }
+        public static void Info(string format, params object?[]? arg) { Log("INFO:   " + string.Format(format, arg), LogLevel.Info); }
+        public static void Warn(string message) { Log("WARN:   " + message, LogLevel.Warn); }
+        public static void Warn(string format, params object?[]? arg) { Log("WARN:   " + string.Format(format, arg), LogLevel.Warn); }
+        public static void Error(string message) { Log("Error: " + message, LogLevel.Error); }
+        public static void Error(string format, params object?[]? arg) { Log("Error: " + string.Format(format, arg), LogLevel.Error); }
+
+        public static void Log(string message, LogLevel prio)
+        {
+            if (prio < Defaut) return;
+            instance.sinks.ForEach(s => s.Log(message, prio));
+        }
+        public static List<ILogSink> CurrentSinks()
+        {
+            return instance.sinks;
+        }
+        public static void AddSink(ILogSink sink)
+        {
+            instance.sinks.Add(sink);
+        }
+        public static void RemoveSink(ILogSink sink)
+        {
+            instance.sinks.Remove(sink);
+        }
+
+
+        private Logger()
+        {
+#if DEBUG
+            sinks.Add(new ConsoleLogger(LogLevel.Debug));
+#endif
+            sinks.Add(new MessageBoxLogger());
+        }
+        private static Logger instance = new Logger();
+        private List<ILogSink> sinks = new List<ILogSink>();
+
+    }
+}
---
+++ doot-gen/util/Sinks.cs
@@ -0,0 +1,150 @@
+﻿using Optional;
+
+namespace doot_gen.util
+{
+    enum LogLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warn = 2,
+        Error = 3
+    }
+
+    interface ILogSink
+    {
+        public abstract LogLevel GetLogLevel();
+        public abstract void OverrideLogLevel(LogLevel newLogLevel);
+        public abstract void Log(string message, LogLevel prio);
+    }
+
+    class ConsoleLogger : ILogSink
+    {
+        private LogLevel _logLevel;
+        public ConsoleLogger(LogLevel initalLogLevel)
+        {
+            _logLevel = initalLogLevel;
+        }
+
+        public LogLevel GetLogLevel()
+        {
+            return _logLevel;
+        }
+
+        public void OverrideLogLevel(LogLevel newLogLevel)
+        {
+            _logLevel = newLogLevel;
+        }
+
+        public void Log(string msg, LogLevel prio)
+        {
+            if (prio < _logLevel) return;
+            Console.WriteLine(msg);
+        }
+    }
+
+    class FileLogger : ILogSink
+    {
+        private LogLevel _logLevel;
+        private string _fileLocation;
+        public FileLogger(string fileLocation, LogLevel initalLogLevel, Option<string> logHeader)
+        {
+            if (File.Exists(fileLocation)) File.Delete(fileLocation);
+            _logLevel = initalLogLevel;
+            _fileLocation = fileLocation;
+
+            try
+            {
+                using (StreamWriter w = File.AppendText(_fileLocation))
+                {
+                    logHeader.DoIfPresent(str => w.WriteLine(str));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("FileLogger failed to write to file: {}", ex.Message);
+            }
+        }
+        public FileLogger(LogLevel initalLogLevel, Option<string> logHeader)
+        {
+            _logLevel = initalLogLevel;
+            _fileLocation = "log-" + DateTime.Now.ToString("HH-mm-ss") + ".txt";
+
+            try
+            {
+                using (StreamWriter w = File.AppendText(_fileLocation))
+                {
+                    logHeader.DoIfPresent(str => w.WriteLine(str));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("FileLogger failed to write to file: {}", ex.Message);
+            }
+        }
+
+        public LogLevel GetLogLevel()
+        {
+            return _logLevel;
+        }
+
+        public void OverrideLogLevel(LogLevel newLogLevel)
+        {
+            _logLevel = newLogLevel;
+        }
+
+        public void Log(string msg, LogLevel prio)
+        {
+            if (prio < _logLevel) return;
+            try
+            {
+                using (StreamWriter w = File.AppendText(_fileLocation))
+                {
+                    w.WriteLine(msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("FileLogger failed to write to file: {}", ex.Message);
+            }
+        }
+    }
+
+    class MessageBoxLogger : ILogSink
+    {
+        private LogLevel _logLevel;
+        public MessageBoxLogger(LogLevel initalLogLevel = LogLevel.Warn)
+        {
+            _logLevel = initalLogLevel;
+        }
+
+        public LogLevel GetLogLevel()
+        {
+            return _logLevel;
+        }
+
+        public void OverrideLogLevel(LogLevel newLogLevel)
+        {
+            _logLevel = newLogLevel;
+        }
+
+        public void Log(string msg, LogLevel prio)
+        {
+            if (prio < _logLevel) return;
+            string titel = "DootGen-" + prio.ToString();
+            string additonalMsg = prio == LogLevel.Error ? "\nYou can summit an error report as an issue on\n https://github.com/Darkblizzard21/mh-doot-gen/issues/new" : "";
+            MessageBoxIcon icon = prio switch
+            {
+                LogLevel.Debug => MessageBoxIcon.Information,
+                LogLevel.Info => MessageBoxIcon.Information,
+                LogLevel.Warn => MessageBoxIcon.Warning,
+                LogLevel.Error => MessageBoxIcon.Error,
+                _ => throw new NotImplementedException()
+            };
+            MessageBox.Show(
+                       msg + additonalMsg, 
+                       titel,
+                       MessageBoxButtons.OK,
+                       icon);
+        }
+    }
+}
