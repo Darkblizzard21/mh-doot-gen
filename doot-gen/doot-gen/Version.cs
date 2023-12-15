3eeda18b441e6cb9087a64899077de7cd414f472
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2023-12-16 00:20:19 +0100

    Updated version

--- doot-gen/doot-gen/Version.cs
+++ doot-gen/doot-gen/Version.cs
@@ -3,7 +3,7 @@ namespace doot_gen
     public static class Version
     {
         public const int VERSION_MAJOR = 1;
-        public const int VERSION_MINOR = 0;
+        public const int VERSION_MINOR = 1;
         public const int VERSION_PATCH = 0;
 
         public static string String()
